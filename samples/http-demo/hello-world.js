25fd0a6356270a2e72d7b82770f36055d952b672
waylau <waylau521@gmail.com>
2019-05-24 20:11:08 +0800

    add http demo. Hello world~

---
+++ samples/http-demo/hello-world.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`服务器运行在 http://${hostname}:${port}/`);
+});
